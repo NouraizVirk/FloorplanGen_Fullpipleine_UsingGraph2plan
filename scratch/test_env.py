e09f28d3a35854cf6162fa714087562e8c02bca1
User <user@example.com>
2026-05-03 01:46:50 +0500

    Initial commit: Lean codebase for GitHub (Excluding 6GB of env/data)

--- scratch/test_env.py
+++
@@ -1,5 +0,0 @@
-import sys
-print(sys.version)
-import django
-print(django.get_version())
-print("Success")
--- scratch/test_import_views.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-sys.path.append(r'e:\Projects\FYP\Graph2Plan\Graph2plan\Interface')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'House.settings')
-import django
-django.setup()
-print("Django setup done")
-from Houseweb import views
-print("Views imported")
